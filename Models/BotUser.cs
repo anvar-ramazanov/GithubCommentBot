06992f8c10a6b3cd8345d1c705d1dca427723fb7
gstq <ramazanov.anvar@gmail.com>
2018-05-21 22:52:58 +0500

    Fix model to efcore

--- Models/BotUser.cs
+++ Models/BotUser.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace GithubCommentBot.Models
 {
     public class BotUser
     {
+        [Key]
         public Int64 ChatId { get; set; }
         public String TelegramName { get; set; }
         public String GithubName { get; set; }
